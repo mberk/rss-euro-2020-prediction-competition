3b56c3a0f719bc654eeaf453739b6e756b185aa2
Maurice Berk <maurice@mauriceberk.com>
2021-06-17 12:58:42 +0100

    Add script for scoring submissions

---
+++ score_submissions.py
@@ -0,0 +1,67 @@
+import argparse
+import os
+
+import numpy as np
+import pandas as pd
+
+
+def anonymise_name(name: str) -> str:
+    return ''.join(c for c in name if c.isupper())
+
+
+def score_submissions(path_to_submissions_file: str, path_to_match_results_file: str) -> pd.DataFrame:
+    match_results = pd.read_csv(path_to_match_results_file)
+    mirrored_match_results = pd.DataFrame(
+        {
+            'team1_name': match_results['team2_name'],
+            'team2_name': match_results['team1_name'],
+            'Group': match_results['Group'],
+            'team1_win': match_results['team2_win'],
+            'draw': match_results['draw'],
+            'team2_win': match_results['team1_win']
+        }
+    )
+    submissions = pd.read_excel(path_to_submissions_file, sheet_name=None, engine='openpyxl')
+
+    scores = []
+    for name, submission in submissions.items():
+        if name.startswith('!'):
+            # Ignore malformed submission for now
+            continue
+        df = pd.concat(
+            [
+                match_results.merge(submission, on=['team1_name', 'team2_name', 'Group']),
+                mirrored_match_results.merge(submission, on=['team1_name', 'team2_name', 'Group'])
+            ]
+        )
+        assert len(df) == len(match_results)
+        # Handle probabilities of 0
+        df.loc[df['p_team1_win'] == 0, 'p_team1_win'] = 1e-12
+        df.loc[df['p_team2_win'] == 0, 'p_team2_win'] = 1e-12
+        df.loc[(df['p_draw'] == 0) & (df['Group'] != 'Knockout'), 'p_draw'] = 1e-12
+        # Normalise probabilities
+        df['p_team1_win'] = df['p_team1_win'] / df[['p_team1_win', 'p_team2_win', 'p_draw']].sum(axis=1)
+        df['p_team2_win'] = df['p_team2_win'] / df[['p_team1_win', 'p_team2_win', 'p_draw']].sum(axis=1)
+        df['p_draw'] = df['p_draw'] / df[['p_team1_win', 'p_team2_win', 'p_draw']].sum(axis=1)
+        score = -(np.log(df['p_team1_win']) * df['team1_win'] + np.log(df['p_draw']) * df['draw'] + np.log(df['p_team2_win']) * df['team2_win']).sum()
+        scores.append({'submission': anonymise_name(name), 'score': score})
+
+    scores = pd.DataFrame(scores).sort_values(['score'])
+    return scores
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--submissions-file',required=True)
+    parser.add_argument('--match-results-file', required=True)
+    parser.add_argument('--output-file', required=True)
+
+    args = parser.parse_args()
+
+    results = score_submissions(args.submissions_file, args.match_results_file)
+    results.to_csv(args.output_file, index=False)
+
+
+if __name__ == '__main__':
+    main()
+
